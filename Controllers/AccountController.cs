29ba55b14398d17f774ffe11f933af76f732c1c9
fernastereo <fernandoecueto@gmail.com>
2020-08-29 16:33:26 -0500

    user registration method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NG_Core_Auth.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,5 +22,39 @@ namespace NG_Core_Auth.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        [HttpPost("action")]
+        public async Task<IActionResult> Register ([FromBody] RegisterViewModel formData)
+        {
+            //Will hold all the error related to registration
+            List<string> errorList = new List<string>();
+
+            var user = new IdentityUser
+            {
+                Email = formData.Email,
+                UserName = formData.Username,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var result = await _userManager.CreateAsync(user, formData.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+
+                //Sending confirmation email
+
+                return Ok(new { username = user.UserName, email = user.Email, status = 1, message = "Registration Suceedeed!" });
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+
+            return BadRequest(new JsonResult(errorList));
+        }
+
     }
 }
