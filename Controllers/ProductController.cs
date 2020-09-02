c581f24fc5a2169d537e26832c2f0b12105dc807
fernastereo <fernandoecueto@gmail.com>
2020-09-02 18:25:32 -0500

    creating product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NG_Core_Auth.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NG_Core_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _Db;
+
+        public ProductController(ApplicationDbContext Db)
+        {
+            _Db = Db;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet("[action]")]
+        public IActionResult GetProducts()
+        {
+            return Ok(_Db.Products.ToList());
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
