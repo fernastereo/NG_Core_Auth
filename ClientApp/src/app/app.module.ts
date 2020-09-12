f799a4e83980107e434bd8fc386fc556471f34aa
fernastereo <fernandoecueto@gmail.com>
2020-09-12 17:39:32 -0500

    creating login functionality

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -24,6 +23,7 @@ import { RegisterComponent } from './register/register.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: '', component: HomeComponent, pathMatch: 'full' },
--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../services/account.service';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,52 @@ import { AccountService } from '../services/account.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private acct: AccountService) { }
+  insertForm: FormGroup;
+  userName: FormControl;
+  password: FormControl;
+  returnUrl: string;
+  errorMessage: string;
+  invalidLogin: boolean;
+
+  constructor(private acct: AccountService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  )
+  { }
+
+  onSubmit() {
+    let userLogin = this.insertForm.value;
+
+    this.acct.login(userLogin.userName, userLogin.password).subscribe(result => {
+      let token = (<any>result).token;
+      console.log(token);
+      console.log(result.userRole);
+      console.log('User logged in successfully');
+      console.log(this.returnUrl);
+      this.invalidLogin = false;
+      this.router.navigateByUrl(this.returnUrl);
+    },
+    error => {
+      this.invalidLogin = true;
+      this.errorMessage = "Invalid details supplied - Could not log in"
+      console.log(this.errorMessage);
+    }
+    )
+
+  }
+
 
   ngOnInit() {
+    this.userName = new FormControl('', [Validators.required]);
+    this.password = new FormControl('', [Validators.required]);
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '';
+
+    this.insertForm = this.fb.group({
+      "userName": this.userName,
+      "password": this.password
+    });
   }
 
 }
