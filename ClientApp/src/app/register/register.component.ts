3431a3d96caa94b37a834c018894c1c23777c80f
fernastereo <fernandoecueto@gmail.com>
2020-09-18 18:37:22 -0500

    creating UI for registration form

--- ClientApp/src/app/register/register.component.ts
+++ ClientApp/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { AccountService } from '../services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private acct: AccountService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  //Properties
+  insertForm: FormGroup;
+  userName: FormControl;
+  password: FormControl;
+  cPassword: FormControl;
+  email: FormControl;
 }
